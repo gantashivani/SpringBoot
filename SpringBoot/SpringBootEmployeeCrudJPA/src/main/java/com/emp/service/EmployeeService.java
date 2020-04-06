5ad8cfdc392ad3b3ec15b3cbc01dd4c40cb3e003
Shivani <ganta.shivani5@gmail.com>
2020-04-06 13:57:02 +0530

    first commit

---
+++ SpringBoot/SpringBootEmployeeCrudJPA/src/main/java/com/emp/service/EmployeeService.java
@@ -0,0 +1,20 @@
+package com.emp.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.emp.entity.Employee;
+
+public interface EmployeeService {
+
+	String createEmp(Employee employee);
+
+	Optional<Employee> findEmpById(Integer empId);
+
+	List<Employee> findAllEmployees();
+
+	String updateEmp(Employee employee);
+
+	String deleteEmp(Integer empId);
+
+}
